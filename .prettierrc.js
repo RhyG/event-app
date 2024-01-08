7a2f3b52c9f6b17899a06cab171deae97486eec8
rhyg <rhysgeary@gmail.com>
2024-01-08 19:33:01 +1000

    Initial setup.

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  importOrder: ['<THIRD_PARTY_MODULES>', '^@/domains/(.*)$', '^@/core/(.*)$', '^@/ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  printWidth: 160,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
