a53226191af8e761d20544b1fc6304b67b9b27ce
rhyg <rhysgeary@gmail.com>
2024-01-20 07:30:37 +1000

    Added theme barrel.

---
+++ app/core/ui/theme/index.ts
@@ -0,0 +1,6 @@
+export { default as colours } from './colours';
+export * from './layout';
+export * from './spacing';
+export * from './timing';
+export * from './typography';
+export * from './theme';
