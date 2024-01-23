d84aab4baea55acabb471d8dfb5a9e89264bab48
rhyg <rhysgeary@gmail.com>
2024-01-24 08:11:21 +1000

    Join event edge function.

---
+++ supabase/functions/join-event/index.ts
@@ -0,0 +1,68 @@
+import * as bcrypt from 'https://deno.land/x/bcrypt@v0.4.1/mod.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+};
+
+Deno.serve(async req => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
+  }
+
+  try {
+    const authHeader = req.headers.get('Authorization')!;
+
+    if (!authHeader) {
+      return new Response(JSON.stringify({ error: 'No authorization header passed' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    const supabaseClient = createClient(Deno.env.get('SUPABASE_URL') ?? '', Deno.env.get('SUPABASE_ANON_KEY') ?? '', {
+      global: { headers: { Authorization: authHeader } },
+    });
+
+    const { code, password } = await req.json();
+
+    const { data, error } = await supabaseClient.from('Events').select('id,password,event_name').limit(1).eq('access_code', code);
+
+    if (error) throw error;
+
+    const result = bcrypt.compareSync(password, data[0].password);
+
+    if (result === false) {
+      return new Response(JSON.stringify({ error: 'Incorrect password' }), { status: 401, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    });
+  } catch (error) {
+    // @ts-expect-error
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/create-event' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
+// Deno.serve(async req => {
+//   const { name } = await req.json();
+//   const data = {
+//     message: `Hello ${name}!`,
+//   };
+
+//   return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
+// });
