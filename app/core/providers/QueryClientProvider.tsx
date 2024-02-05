6e0beb9d81c4dbc77fbb4d3e5b9d6127e434371c
rhyg <rhysgeary@gmail.com>
2024-02-06 08:19:22 +1000

    Add window focus logic to tanstack provider.

--- app/core/providers/QueryClientProvider.tsx
+++ app/core/providers/QueryClientProvider.tsx
@@ -1,9 +1,20 @@
-import { QueryClient, QueryClientProvider as _QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider as _QueryClientProvider, focusManager } from '@tanstack/react-query';
+import { PropsWithChildren, useEffect } from 'react';
+import { AppState, AppStateStatus, Platform } from 'react-native';
 
 // Create a client
 export const queryClient = new QueryClient();
 
 export function QueryClientProvider({ children }: PropsWithChildren<Record<string, unknown>>) {
+  useEffect(function manageWindowFocus() {
+    const subscription = AppState.addEventListener('change', function onAppStateChange(status: AppStateStatus) {
+      if (Platform.OS !== 'web') {
+        focusManager.setFocused(status === 'active');
+      }
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   return <_QueryClientProvider client={queryClient}>{children}</_QueryClientProvider>;
 }
