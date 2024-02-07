6ae96c8b759516d0122615021950771fef26fcc6
rhyg <rhysgeary@gmail.com>
2024-02-08 06:54:37 +1000

    Removed cloudinary.

--- app/core/lib/cloudinary.ts
+++
@@ -1,5 +0,0 @@
-import { Cloudinary } from '@cloudinary/url-gen';
-
-const cloudName = process.env.EXPO_PUBLIC_CLOUD_NAME;
-
-export const cloudinary = new Cloudinary({ cloud: { cloudName, apiKey: '121569739225482', apiSecret: 'XDDARPcs1U96DjWjvElCnkTcMsw' }, url: { secure: true } });
--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -3,7 +3,6 @@ declare global {
     interface ProcessEnv {
       EXPO_PUBLIC_SUPABASE_URL: string;
       EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
-      EXPO_PUBLIC_CLOUD_NAME: string;
       [key: string]: string | undefined;
     }
   }
