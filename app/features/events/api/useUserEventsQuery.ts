359b596e22c27648c162e95cf70b140f7559ffad
rhyg <rhysgeary@gmail.com>
2024-02-12 07:53:48 +1000

    Previous and upcoming events selectors.

--- app/features/events/api/useUserEventsQuery.ts
+++ app/features/events/api/useUserEventsQuery.ts
@@ -2,12 +2,26 @@ import { useQuery } from '@tanstack/react-query';
 
 import { useUserContext } from '@feature/user';
 
+import { Event } from '../types';
 import { EventsAPI } from './EventsAPI';
 
 const userEventsQueryKey = ['events'];
 
-export function useUserEventsQuery() {
+const selectors = {
+  selectPreviousEvents: (events: Event[]) => events.filter(event => new Date(event.event_date) < new Date()),
+  selectUpcomingEvents: (events: Event[]) => events.filter(event => new Date(event.event_date) >= new Date()),
+};
+
+export function useUserEventsQuery(selector?: (events: Event[]) => Event[]) {
   const { user } = useUserContext();
 
-  return useQuery({ queryKey: userEventsQueryKey, queryFn: () => EventsAPI.getUserEvents(user?.id ?? '') });
+  return useQuery({ queryKey: userEventsQueryKey, queryFn: () => EventsAPI.getUserEvents(user?.id ?? ''), select: selector });
+}
+
+export function usePreviousEventsQuery() {
+  return useUserEventsQuery(selectors.selectPreviousEvents);
+}
+
+export function useUpcomingEventsQuery() {
+  return useUserEventsQuery(selectors.selectUpcomingEvents);
 }
