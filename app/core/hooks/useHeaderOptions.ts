8d3c0a431d4431e1f65c9206ee73d58c2c5cdb1a
rhyg <rhysgeary@gmail.com>
2024-02-15 06:18:31 +1000

    Fixed `useHeaderOptions` types.

--- app/core/hooks/useHeaderOptions.ts
+++ app/core/hooks/useHeaderOptions.ts
@@ -1,7 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { useLayoutEffect } from 'react';
 
-export function useHeaderOptions(options: Record<string, unknown>) {
+export function useHeaderOptions(options: NativeStackNavigationOptions) {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
