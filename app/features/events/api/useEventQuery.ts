02d9b0be4c5a21c3a6a55b470235d052b0043488
rhyg <rhysgeary@gmail.com>
2024-02-26 08:18:35 +1000

    Juiced up `useEventQuery`.

--- app/features/events/api/useEventQuery.ts
+++ app/features/events/api/useEventQuery.ts
@@ -1,12 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
 
 import { getEventPhotos } from '@feature/photo-management/services/PhotoService';
 
+import { EventById } from '../types';
 import { EventsAPI } from './EventsAPI';
 import { eventDetailsQueryKey, eventPhotosQueryKey } from './query-keys';
 
-export function useEventDetailsQuery(eventId: string) {
-  return useQuery({ queryKey: eventDetailsQueryKey(eventId), queryFn: () => EventsAPI.getEventById(eventId), staleTime: 60_000, refetchOnWindowFocus: true });
+export function useEventDetailsQuery<TSelector = undefined>(
+  eventId: string,
+  selector?: (data: EventById) => TSelector,
+): UseQueryResult<TSelector extends undefined ? EventById : TSelector> {
+  return useQuery({
+    queryKey: eventDetailsQueryKey(eventId),
+    queryFn: () => EventsAPI.getEventById(eventId),
+    staleTime: 60_000,
+    refetchOnWindowFocus: true,
+    select: selector as unknown as (data: any) => any, // Type assertion to satisfy TS compiler.
+  });
 }
 
 export function useEventPhotosQuery(eventId: string) {
