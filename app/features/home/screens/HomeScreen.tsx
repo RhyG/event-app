cc5c863bd8d4625a00417291fbeec06c7dc7cfda
rhyg <rhysgeary@gmail.com>
2024-03-18 21:11:07 +1000

    WIP empty home screen.

--- app/features/home/screens/HomeScreen.tsx
+++ app/features/home/screens/HomeScreen.tsx
@@ -4,16 +4,31 @@ import { StyleSheet } from 'react-native';
 import { ScreenProp } from '@app/navigation/types';
 
 import { CreateEventScreenName, JoinEventScreenName } from '@feature/events';
+import { useAllEventsQuery } from '@feature/events/api/useUserEventsQuery';
 
 import { Button } from '@ui/components/Button';
 import { Icon } from '@ui/components/Icon';
 import { Screen } from '@ui/components/Screen';
-import { HBox } from '@ui/components/layout/Box';
+import { Text } from '@ui/components/Text';
+import { HBox, VBox } from '@ui/components/layout/Box';
 import { useThemedStyles } from '@ui/theme/useThemedStyles';
 
 import { useHomeHeader } from '../hooks/useHomeHeader';
 import { PreviousEventsSection } from './components/PreviousEventsSection';
 import { UpcomingEventsSection } from './components/UpcomingEventsSection';
+
+function NoEventsView({ navigateToCreateEvent, navigateToJoinEvent }: { navigateToCreateEvent: () => void; navigateToJoinEvent: () => void }) {
+  return (
+    <VBox ph="base" justifyContent="center" flex={1}>
+      <Text align="center" preset="subheading">
+        You haven't joined any events yet
+      </Text>
+      <Text align="center">Join an event or create a new one</Text>
+      <CreateEventButton onPress={navigateToCreateEvent} />
+      <JoinEventButton onPress={navigateToJoinEvent} />
+    </VBox>
+  );
+}
 
 function JoinEventButton({ onPress }: { onPress: () => void }) {
   const { styles } = useThemedStyles(stylesFn);
@@ -38,15 +53,32 @@ function CreateEventButton({ onPress }: { onPress: () => void }) {
 export function HomeScreen({ navigation }: ScreenProp<'HomeScreen'>) {
   useHomeHeader();
 
+  const { data } = useAllEventsQuery();
+  const userHasNoEvents = data?.length === 0;
+
+  function navigateToCreateEvent() {
+    navigation.navigate(CreateEventScreenName);
+  }
+
+  function navigateToJoinEvent() {
+    navigation.navigate(JoinEventScreenName);
+  }
+
   return (
     <Screen style={{ flex: 1 }}>
-      <HBox mv="base" gap="small">
-        <JoinEventButton onPress={() => navigation.navigate(JoinEventScreenName)} />
-        <CreateEventButton onPress={() => navigation.navigate(CreateEventScreenName)} />
-      </HBox>
+      {userHasNoEvents ? (
+        <NoEventsView navigateToCreateEvent={navigateToCreateEvent} navigateToJoinEvent={navigateToJoinEvent} />
+      ) : (
+        <>
+          <HBox mv="base" gap="small">
+            <JoinEventButton onPress={navigateToJoinEvent} />
+            <CreateEventButton onPress={navigateToCreateEvent} />
+          </HBox>
 
-      <UpcomingEventsSection />
-      <PreviousEventsSection />
+          <UpcomingEventsSection />
+          <PreviousEventsSection />
+        </>
+      )}
     </Screen>
   );
 }
