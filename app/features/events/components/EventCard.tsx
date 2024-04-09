73cccea3dc465a3e74d3f5bc858a1c8fa70fd8e5
rhyg <rhysgeary@gmail.com>
2024-04-10 08:23:16 +1000

    More event card fixes.

--- app/features/events/components/EventCard.tsx
+++ app/features/events/components/EventCard.tsx
@@ -2,6 +2,8 @@ import { useNavigation } from '@react-navigation/native';
 import { Image } from 'expo-image';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
+import { formatTimestamp, isToday } from '@core/lib/date';
+
 import { Button } from '@ui/components/Button';
 import { Icon } from '@ui/components/Icon';
 import { Text } from '@ui/components/Text';
@@ -9,9 +11,9 @@ import { HBox, VBox } from '@ui/components/layout/Box';
 import { Theme } from '@ui/theme/theme';
 import { useThemedStyles } from '@ui/theme/useThemedStyles';
 
+import { useEventPreviewImageQuery } from '../api/useEventPreviewQuery';
 import { EventScreenName } from '../screens/EventScreen/EventScreen';
 import { Event } from '../types';
-import { useEventCard } from './EventCard.hooks';
 
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
@@ -19,18 +21,29 @@ const blurhash =
 const defaultImage =
   'https://images.unsplash.com/photo-1501238295340-c810d3c156d2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
+export function EventCardImage({ photoURL }: { photoURL: string }) {
+  const { styles } = useThemedStyles(stylesFn);
+
+  const { data } = useEventPreviewImageQuery({ photoURL, enabled: !!photoURL });
+  const previewImage = { signedUrl: data };
+
+  return (
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} source={previewImage?.signedUrl ?? defaultImage} contentFit="cover" transition={1000} placeholder={blurhash} />
+    </View>
+  );
+}
+
 export function EventCard({ event_name, event_date, preview_url, id }: Event) {
   const navigation = useNavigation();
 
   const { styles, theme } = useThemedStyles(stylesFn);
 
-  const { previewImage, formattedDate } = useEventCard(preview_url, event_date);
+  const formattedDate = isToday(event_date) ? 'Today' : formatTimestamp(event_date);
 
   return (
     <TouchableOpacity style={styles.eventContainer} onPress={() => navigation.navigate(EventScreenName, { id, name: event_name })} key={id}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={previewImage?.signedUrl ?? defaultImage} contentFit="cover" transition={1000} placeholder={blurhash} />
-      </View>
+      {preview_url ? <EventCardImage photoURL={preview_url} /> : null}
 
       <VBox p="extraSmall">
         <HBox justifyContent="space-between" alignItems="center">
