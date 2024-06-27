35e3b8078a0827fd6e7e12b5da50bb64a90b7dc3
rhyg <rhysgeary@gmail.com>
2024-06-27 21:00:32 +1000

    Returning to screens object

---
+++ app/navigation/screens.ts
@@ -0,0 +1,37 @@
+import { AccountScreenName } from '@feature/account';
+import { QRCodeScannerScreenName } from '@feature/code-scanning/screens/QRCodeScannerScreen/QRCodeScannerScreen';
+import { CreateEventScreenName, JoinEventScreenName } from '@feature/events';
+import { ConfirmPhotosScreenName } from '@feature/events/screens/ConfirmPhotosScreen/ConfirmPhotosScreen';
+import { EditEventScreenName } from '@feature/events/screens/EditEventScreen/EditEventScreen';
+import { EventScreenName } from '@feature/events/screens/EventScreen/EventScreen';
+import { PhotoCarouselScreenName } from '@feature/events/screens/PhotoCarouselScreen/PhotoCarouselScreen';
+import { HomeScreenName } from '@feature/home/screens/HomeScreen/HomeScreen';
+import { EmailLoginScreenName } from '@feature/onboarding/screens/EmailLoginScreen/EmailLoginScreen';
+import { EmailSignUpScreenName } from '@feature/onboarding/screens/EmailSignUpScreen/EmailSignUpScreen';
+import { ResetPasswordScreenName } from '@feature/onboarding/screens/ResetPasswordScreen/ResetPasswordScreen';
+import { WelcomeScreenName } from '@feature/onboarding/screens/WelcomeScreen/WelcomeScreen';
+
+import type { RootStackParamList } from './types';
+
+export const Screens: { [K in keyof RootStackParamList]: K } = {
+  TabNavigator: 'TabNavigator',
+  CreateEventNavigator: 'CreateEventNavigator',
+
+  WelcomeNavigator: 'WelcomeNavigator',
+  WelcomeScreen: WelcomeScreenName,
+  EmailLoginScreen: EmailLoginScreenName,
+  EmailSignUpScreen: EmailSignUpScreenName,
+  ResetPasswordScreen: ResetPasswordScreenName,
+
+  HomeScreen: HomeScreenName,
+  JoinEventScreen: JoinEventScreenName,
+  CreateEventScreen: CreateEventScreenName,
+  EventScreen: EventScreenName,
+  QRCodeScannerScreen: QRCodeScannerScreenName,
+  AccountScreen: AccountScreenName,
+  ConfirmPhotosScreen: ConfirmPhotosScreenName,
+  EditEventScreen: EditEventScreenName,
+  PhotoCarouselScreen: PhotoCarouselScreenName,
+} as const;
+
+export type Screen = keyof typeof Screens;
